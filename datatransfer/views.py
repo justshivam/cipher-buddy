3ba215433d0f1c0361dc54a42c77e263d3dd96e8
justshivam <stdnt.shivam@gmail.com>
2021-03-23 09:25:08 +0530

    Connect Portal to P2P

--- datatransfer/views.py
+++ datatransfer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'datatransfer/index.html')
+def peer(request):
+    return render(request, 'datatransfer/peer.html')
