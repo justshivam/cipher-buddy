156d813b8bc68d28f9918fa3912db75f1c54a929
justshivam <stdnt.shivam@gmail.com>
2021-03-23 09:08:19 +0530

    Portal App Connected

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('home', views.home),
+    path('contact', views.contact),
+    path('about', views.about),
+    path('complaint', views.complaint),
+    path('status', views.status)
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
-def index(request):
-    pass
+def home(request):
+    return render(request, 'portal/home.html')
+
+
+def complaint(request):
+    return render(request, 'portal/complaint.html')
+
+
+def contact(request):
+    return render(request, 'portal/contactUs.html')
+
+
+def about(request):
+    return render(request, 'portal/aboutUs.html')
+
+
+def status(request):
+    return render(request, 'portal/status.html')
