f9f2c19c274bcd2acb935ad4472990e19f70f06a
justshivam <stdnt.shivam@gmail.com>
2021-03-26 16:22:57 +0530

    Basic Login Activity

--- login/views.py
+++ login/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 def login(request):
-    return render(request, 'login/login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/portal/home')
+        else:
+            messages.info(request, 'Invalid credentials.')
+            return redirect('/')
+    else:
+        return render(request, 'login/login.html')
 
 
 def createUser(request):
-    return render(request, 'login/createUser.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password_c = request.POST['password_repeat']
+
+        if password == password_c:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "User already exists.")
+                return redirect('/')
+            user = User.objects.create_user(
+                email=email, password=password, first_name=first_name, last_name=last_name, username=email)
+            user.save()
+            print("user created")
+            messages.info(request, 'User created succesfully.')
+            return redirect('/')
+
+        else:
+            print('here')
+            messages.info(request, 'Passwords don\'t match')
+            return redirect('/register')
+    else:
+        print('not here')
+        return render(request, 'login/createUser.html')
--- portal/models.py
+++ portal/models.py
@@ -1,20 +1,2 @@
 from django.db import models
 from . import functions
-
-
-class Email(models.Model):
-    email = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.email
-
-
-class CyberComplaint(models.Model):
-    email = models.ForeignKey(
-        Email, on_delete=models.CASCADE, primary_key=True, unique=True)
-    name = models.CharField(max_length=100)
-    subject = models.CharField(max_length=266)
-    description = models.CharField(max_length=1000)
-
-    def __str__(self):
-        return self.subject
