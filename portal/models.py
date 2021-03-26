bb87ee60c470a6c90dff2db1f10b16eef72b119b
justshivam <stdnt.shivam@gmail.com>
2021-03-26 22:16:09 +0530

    Databse Ready

--- portal/models.py
+++ portal/models.py
@@ -1,2 +1,31 @@
 from django.db import models
-from . import functions
+
+
+class Email(models.Model):
+    email = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.email
+
+
+class Complaint(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    subject = models.CharField(max_length=266)
+    details = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.subject
+
+
+class SiteComplaint(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    subject = models.CharField(max_length=266)
+    details = models.CharField(max_length=1000)
+    website = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.website} - {self.subject}'
--- portal/views.py
+++ portal/views.py
@@ -1,8 +1,27 @@
+from portal.models import Complaint, Email
+from django.contrib import messages
 from django.shortcuts import redirect, render
+from .models import *
 
 
 def home(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            name = request.POST['name']
+            phone = request.POST['phone']
+            email = request.user.username
+            subject = request.POST['subject']
+            details = request.POST['details']
+            if len(phone) != 10:
+                messages.info(request, "Invalid phone number.")
+                return redirect('/portal/home')
+            if not Email.objects.filter(email=email).exists():
+                x = Email.objects.create(email=email)
+            x = Complaint(Email.objects.get(email=email), name=name,
+                          phone=phone, subject=subject, details=details)
+            x.save()
+            messages.info("Submitted successfully")
+            return redirect('/portal/home')
         return render(request, 'portal/home.html')
     else:
         return redirect('/')
@@ -10,6 +29,23 @@ def home(request):
 
 def complaint(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            name = request.POST['name']
+            phone = request.POST['phone']
+            email = request.user.username
+            subject = request.POST['subject']
+            details = request.POST['details']
+            website = request.POST['site']
+            if len(phone) != 10:
+                messages.info(request, "Invalid phone number.")
+                return redirect('/portal/complaint')
+            if not Email.objects.filter(email=email).exists():
+                x = Email.objects.create(email=email)
+            x = SiteComplaint(Email.objects.get(email=email), name=name,
+                              phone=phone, subject=subject, details=details, website=website)
+            x.save()
+            messages.info("Submitted successfully")
+            return redirect('/portal/complaint')
         return render(request, 'portal/complaint.html')
     else:
         return redirect('/')
