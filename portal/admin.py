b19516e13a66bdde4a6bd273509e66a113176b17
justshivam <stdnt.shivam@gmail.com>
2021-03-27 14:27:53 +0530

    Update admin.py

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    pass
+
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+class SiteComplaintsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Email, EmailAdmin)
+admin.site.register(models.Complaint, ComplaintAdmin)
+admin.site.register(models.Contact, ContactAdmin)
+admin.site.register(models.SiteComplaint, SiteComplaintsAdmin)
